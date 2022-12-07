ec2070cc8e02b2be9fb865987a25a8d77193fd80
hyun <hyun.choi499@gmail.com>
2022-12-07 11:01:31 +0900

    JPA는 객체 중심 쿼리 작성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -9,37 +11,23 @@ import org.hibernate.HibernateException;
 
 public class JpaMain {
 	public static void main(String[] args) {
-		// EntityManagerFactory - 애플리케이션당 하나
-		// persistence.xml에 선언되어 있는 persistence-unit 값 전달
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-		// EntityManager - 하나의 작업 진행을 하기위해 얻고 작업 후 닫아야함
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager em = emf.createEntityManager();
 
-		// code
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
 
 		try {
-			// insert 작업
-//			Member member = new Member(1L, "HelloA");
-//			em.persist(member);
+			// JPA는 객체를 중심으로 쿼리 작성하는 미묘한 차이가 있음
+//			List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+			List<Member> result = em.createQuery("select m from Member as m", Member.class).setFirstResult(5)
+					.setMaxResults(8).getResultList();
+			// persistence.xml의 hibernate.dialect를 다른 db로 바꾸면 자동으로 해당 db에 맞는 쿼리를 날려준다.
 
-			// select 작업
-//			Member findMember = em.find(Member.class, 1L);
-//			System.out.println(findMember.toString());
-
-			// delete 작업
-//			Member find = em.find(Member.class, 1L);
-//			em.remove(find);
-
-			// update 작업
-			// update시 em.persist()는 필요 x
-//			Member findMember = em.find(Member.class, 1L);
-//			findMember.setName("HelloJPA");
-			// find를 통해 가져온 객체(em.find에 해당)는 트랜잭션 commit시에
-			// 객체의 필드 변경을 알아서 체크하여 자동으로 update 쿼리를 추가로 넣어준다.
-
+			for (Member member : result) {
+				System.out.println(member.toString());
+			}
 			tx.commit();
 
 		} catch (HibernateException e) {
