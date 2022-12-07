0bb2e99e5bf465ccb128a9ee54f7482594418705
hyun <hyun.choi499@gmail.com>
2022-12-07 14:16:42 +0900

    영속성 컨텍스트 비움 clear()

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,13 +18,15 @@ public class JpaMain {
 
 		try {
 
-			Member member = em.find(Member.class, 150L); 
+			Member member = em.find(Member.class, 150L);
 			member.setName("변경");
-			
-			//member는 준영속 상태가됨 (영속상태에서 비영속상태가 된경우 준영속이라 부름)
-			//영속성 컨텍스트에서 member만 준영속 상태로 변경
-			em.detach(member);
-			
+
+			// 영속성 컨텍스트를 통으로 날림
+			em.clear();
+
+			// 컨텍스트가 비워졌기 때문에 1차 캐시에 없어서 select 쿼리가 다시 나감
+			em.find(Member.class, 150L);
+
 			System.out.println("============");
 
 			tx.commit();
