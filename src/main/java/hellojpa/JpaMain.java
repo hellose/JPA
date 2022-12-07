76e281927085e11960db1378b10032d03b75dd41
hyun <hyun.choi499@gmail.com>
2022-12-07 14:47:48 +0900

    데이터베이스 스키마 자동 생성 create시 테스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,6 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import org.hibernate.HibernateException;
@@ -11,30 +9,21 @@ public class JpaMain {
 	public static void main(String[] args) {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-		EntityManager em = emf.createEntityManager();
+//		EntityManager em = emf.createEntityManager();
 
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
+//		EntityTransaction tx = em.getTransaction();
+//		tx.begin();
 
 		try {
 
-			Member member = em.find(Member.class, 150L);
-			member.setName("변경");
+		
 
-			// 영속성 컨텍스트를 통으로 날림
-			em.clear();
-
-			// 컨텍스트가 비워졌기 때문에 1차 캐시에 없어서 select 쿼리가 다시 나감
-			em.find(Member.class, 150L);
-
-			System.out.println("============");
-
-			tx.commit();
+//			tx.commit();
 
 		} catch (HibernateException e) {
-			tx.rollback();
+//			tx.rollback();
 		} finally {
-			em.close();
+//			em.close();
 		}
 
 		emf.close();
