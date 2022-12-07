11c6aaf45cf1cd3ebd3f2fdcf5e7301e4ec41f6e
hyun <hyun.choi499@gmail.com>
2022-12-07 14:14:28 +0900

    영속성 컨텍스트에서 특정 엔티티만 준영속 상태로 변경

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,5 @@
 package hellojpa;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -20,18 +18,13 @@ public class JpaMain {
 
 		try {
 
-			Member member1001 = new Member(1001L, "member1001");
-			Member member1002 = new Member(1002L, "member1002");
-			Member member1003 = new Member(1003L, "member1003");
-
-			em.persist(member1001);
-			em.persist(member1002);
-			em.persist(member1003);
-
-			// JPQL 쿼리 실행시 flush()가 자동으로 호출되는 이유
-			// <- 쓰기 지연된 내용이 모두 DB에 반영해야 DB쿼리 호출시 정확한 정보을 얻어올수 있기 떄문에
-			List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
-
+			Member member = em.find(Member.class, 150L); 
+			member.setName("변경");
+			
+			//member는 준영속 상태가됨 (영속상태에서 비영속상태가 된경우 준영속이라 부름)
+			//영속성 컨텍스트에서 member만 준영속 상태로 변경
+			em.detach(member);
+			
 			System.out.println("============");
 
 			tx.commit();
