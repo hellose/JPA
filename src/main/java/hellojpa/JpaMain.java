34990f5f82229f4bf97ef1d9026c01023c27ce2e
hyun <hyun.choi499@gmail.com>
2022-12-07 13:40:57 +0900

    JPQL실행시 flush()가 자동으로 호출되는 이유

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -17,23 +19,21 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			//EntityTransaction이 commit되면 플러시가 발생
-			// -> 변경 감지, 수정된 엔티티 쓰기 지연 SQL 저장소에 등록 -> 실제 db commit
-			
-			//직접 플러시를 쓸일은 별로 없지만 알아놔야 좋음
-			
-			//직접 호출하는 경우 - em.flush()
-			//자동 호출되는 경우 - em.commit() 또는 JPQL쿼리실행
-			
-			Member member = new Member(200L, "member200");
-			em.persist(member);
-			
-			//이렇게 할 경우 여기서 insert쿼리가 나감
-			//중요한 것은 flush()시 1차 캐시가 날라가는게 아니라 최신 정보상태를 유지하고 있음
-			em.flush();
-			
+
+			Member member1001 = new Member(1001L, "member1001");
+			Member member1002 = new Member(1002L, "member1002");
+			Member member1003 = new Member(1003L, "member1003");
+
+			em.persist(member1001);
+			em.persist(member1002);
+			em.persist(member1003);
+
+			// JPQL 쿼리 실행시 flush()가 자동으로 호출되는 이유
+			// <- 쓰기 지연된 내용이 모두 DB에 반영해야 DB쿼리 호출시 정확한 정보을 얻어올수 있기 떄문에
+			List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+
 			System.out.println("============");
-			
+
 			tx.commit();
 
 		} catch (HibernateException e) {
