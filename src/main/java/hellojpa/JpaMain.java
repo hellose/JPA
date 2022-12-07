20128a93bcd25737ffc7c7ef9d8ebedefaa58420
hyun <hyun.choi499@gmail.com>
2022-12-07 13:26:54 +0900

    영속성 컨텍스트 flush

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,11 +17,20 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-
-			Member member = em.find(Member.class, 150L);
-			//엔티티의 필드 변경 감지 -> 자동으로 db에 update반영된다. 
-			member.setName("ZZZZ");
-
+			//EntityTransaction이 commit되면 플러시가 발생
+			// -> 변경 감지, 수정된 엔티티 쓰기 지연 SQL 저장소에 등록 -> 실제 db commit
+			
+			//직접 플러시를 쓸일은 별로 없지만 알아놔야 좋음
+			
+			//직접 호출하는 경우 - em.flush()
+			//자동 호출되는 경우 - em.commit() 또는 JPQL쿼리실행
+			
+			Member member = new Member(200L, "member200");
+			em.persist(member);
+			
+			//이렇게 할 경우 여기서 insert쿼리가 나감
+			em.flush();
+			
 			System.out.println("============");
 			
 			tx.commit();
