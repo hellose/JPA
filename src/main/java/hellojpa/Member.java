57ed4f662696b01ab8296c5393f55542e9b994a0
hyun <hyun.choi499@gmail.com>
2022-12-08 10:43:01 +0900

    @SequenceGenerator

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,21 +4,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//기본키 맵핑 - @Id, @GeneratedValue
-
 @Entity
 @NoArgsConstructor
 @Setter
 @Getter
+//member_seq_generator
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	//member_seq_generator
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
 	private Long id;
 
 }
